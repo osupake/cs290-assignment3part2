b318a7f65404c9ff78418ed1bc28611623aef6e0
osupake <epak811@gmail.com>
2015-04-22 18:10:26 -0400

    fixed feature to allow 1 to 5 pages of results to display

--- gist.js
+++ gist.js
@@ -1,29 +1,36 @@
-function gists(){
+var results = [];
+
+function startGists() {
+	var pages = 1;
 	var pageNum = document.getElementsByName('pages')[0].value;
-	var totalPages = pageNum * 30;
-	var xhr = new XMLHttpRequest();
-	var url = 'https://api.github.com/gists?per_page=' + totalPages;
-	
-	console.log(totalPages);
 
-	xhr.onreadystatechange = function() {
-		if(xhr.readyState === 4 && xhr.status === 200) {
-			var response = JSON.parse(xhr.responseText);
-			console.log(typeof response);
-			console.log(response);
-			showGists(response);
-		}
+	for(var i=1; i <= pageNum; i++) {
+		var url = 'https://api.github.com/gists?page=' + pages + '&per_page=30';
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = getGists;
+		xhr.open('GET', url);
+		xhr.send();
+		pages++
 	}
-
-	xhr.open('GET', url);
-	xhr.send();
 }
 
-function showGists(response) {
+function getGists() {
+	if(this.readyState === 4 && this.status === 200) {
+		var response = JSON.parse(this.responseText);
+		//console.log(typeof response);
+		//console.log(response);
+		for (var i=0; i < response.length; i++) {
+			results.push(' <a href="' + response[i].html_url + '">' + response[i].description + '</a>');
+		}
+	}
+	showGists(results);
+}
+
+function showGists(array) {
 	var showResults = '<ol>';
-	for(var i=0; i < response.length; i++) {
-		showResults += '<li>' + ' <a href="' +response[i].html_url + '">';
-		showResults += response[i].description + '</a>';
+	for(var i=0; i < array.length; i++) {
+		showResults += '<li>';
+		showResults += array[i];
 		showResults += '</li>';
 	}
 	showResults += '</ol>';
