b93ac8399f021fb4ce666947b118827bfee832d5
osupake <epak811@gmail.com>
2015-04-22 19:26:38 -0400

    removed global variable and fixed results displaying feature

--- gist.js
+++ gist.js
@@ -1,38 +1,35 @@
-var results = [];
-
 function startGists() {
 	var pages = 1;
 	var pageNum = document.getElementsByName('pages')[0].value;
+	var results = [];
 
 	for(var i=1; i <= pageNum; i++) {
 		var url = 'https://api.github.com/gists?page=' + pages + '&per_page=30';
 		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = getGists;
+		xhr.onreadystatechange = function () {
+			if(this.readyState === 4 && this.status === 200) {
+				var response = JSON.parse(this.responseText);
+				//console.log(typeof response);
+				//console.log(response);
+				for (var i=0; i < response.length; i++) {
+					results.push(' <a href="' + response[i].html_url + '">' + response[i].description + '</a>');
+				}
+			}
+			showGists(results);
+		}
 		xhr.open('GET', url);
 		xhr.send();
 		pages++
 	}
 }
 
-function getGists() {
-	if(this.readyState === 4 && this.status === 200) {
-		var response = JSON.parse(this.responseText);
-		//console.log(typeof response);
-		//console.log(response);
-		for (var i=0; i < response.length; i++) {
-			results.push(' <a href="' + response[i].html_url + '">' + response[i].description + '</a>');
-		}
-	}
-	showGists(results);
-}
-
 function showGists(array) {
-	var showResults = '<ol>';
+	var resultsList = '<ol>';
 	for(var i=0; i < array.length; i++) {
-		showResults += '<li>';
-		showResults += array[i];
-		showResults += '</li>';
+		resultsList += '<li>';
+		resultsList += array[i];
+		resultsList += '</li>';
 	}
-	showResults += '</ol>';
-	document.getElementById('results').innerHTML = showResults;
+	resultsList += '</ol>';
+	document.getElementById('results').innerHTML = resultsList;
 }
